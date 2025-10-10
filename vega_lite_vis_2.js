6b357430bf2e474eb738d265b498093a868ba95f
Ann Nguyen <xngu0020@student.monash.edu>
2025-10-10 21:14:09 +1100

    Add files via upload

---
+++ vega_lite_vis_2.js
@@ -0,0 +1,5 @@
+var vg_2 = "gender_ai.vg.json";
+vegaEmbed("#linechart", vg_2).then(function(result) {
+
+// Access the Vega view instance(https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
